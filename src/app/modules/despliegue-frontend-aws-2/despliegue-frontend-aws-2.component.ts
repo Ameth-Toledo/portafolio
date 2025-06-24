d592014d7f8d3ac6b72b885087c0963b7a9d85d0
Ameth-Toledo <233363@ids.upchiapas.edu.mx>
2025-06-24 14:02:20 -0600

    responsive

--- src/app/modules/despliegue-frontend-aws-2/despliegue-frontend-aws-2.component.ts
+++ src/app/modules/despliegue-frontend-aws-2/despliegue-frontend-aws-2.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
-
+import { CardTecnologyComponent } from "../../components/card-tecnology/card-tecnology.component";
+import { TecnologiasComponent } from "../../components/tecnologias/tecnologias.component";
+import { AnimationService } from '../../services/animation/animation.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-despliegue-frontend-aws-2',
   standalone: true,
-  imports: [],
+  imports: [CardTecnologyComponent, TecnologiasComponent],
   templateUrl: './despliegue-frontend-aws-2.component.html',
   styleUrl: './despliegue-frontend-aws-2.component.css'
 })
 export class DespliegueFrontendAws2Component {
+constructor (
+    private router : Router,
+    private animationService: AnimationService
+  ) {}
 
+  ngOnInit() {
+    setTimeout(() => {
+      this.animationService.initAnimations();
+    }, 50);
+  }
+
+  ngOnDestroy(): void {}
+
+  sendToHome(event: Event) {
+    event.preventDefault();
+    this.router.navigate(['']);
+  }
+
+  sendToDeployAWS2(event: Event) {
+    event.preventDefault();
+    this.router.navigate(['despliegue/front/aws/ec2/id=2'])
+  }
 }
